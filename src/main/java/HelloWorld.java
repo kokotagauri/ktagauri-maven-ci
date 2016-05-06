61b67e8205ad6f0c0a42a12be6301d1335b816f4
kokotagauri <ktaga12@freeuni.edu.ge>
2016-05-06 22:09:07 +0400

    Create Maven Project

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Koko on 06.05.2016.
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World !");
+    }
+}
---
+++ src/test/java/TestSimple.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Koko on 06.05.2016.
+ */
+public class TestSimple {
+}
